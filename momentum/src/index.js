add3f968f5ade015b44c12b54a7f33d45b2d1dab
kykysja <kykysja11@gmail.com>
2021-10-21 22:21:08 +0200

    feat: implement background image loading

--- momentum/src/index.js
+++ momentum/src/index.js
@@ -1,7 +1,9 @@
-import { handleTimeChanging } from "./js/calendar";
-
+import { setBackground } from "./js/calendar";
 import { getUserNameFromLocalStorage } from "./js/user-name";
 import { setUserNameToLocalStorage } from "./js/user-name";
+import { handleTimeChanging } from "./js/calendar";
+
+setBackground();
 
 window.addEventListener("load", getUserNameFromLocalStorage);
 window.addEventListener("beforeunload", setUserNameToLocalStorage);
--- momentum/src/js/calendar.js
+++ momentum/src/js/calendar.js
@@ -1,23 +1,25 @@
+const body = document.body;
+
 const timeContainer = document.querySelector(".time");
 const dateContainer = document.querySelector(".date");
 const greetingContainer = document.querySelector(".greeting");
 
 export const handleTimeChanging = () => {
-  const date = new Date();
-
-  showTime(date);
-  showDate(date);
-  showGreeting(date);
+  showTime();
+  showDate();
+  showGreeting();
 
   setTimeout(handleTimeChanging, 1);
 };
 
-const showTime = (date) => {
+const showTime = () => {
+  const date = new Date();
   const currentTime = date.toLocaleTimeString();
   timeContainer.textContent = `${currentTime}`;
 };
 
-const showDate = (date) => {
+const showDate = () => {
+  const date = new Date();
   const options = {
     month: "long",
     weekday: "long",
@@ -27,7 +29,8 @@ const showDate = (date) => {
   dateContainer.textContent = `${currentDate}`;
 };
 
-const getTimeOfDay = (date) => {
+export const getTimeOfDay = () => {
+  const date = new Date();
   const hours = date.getHours();
 
   if ((hours >= 6) & (hours < 12)) return "morning";
@@ -36,7 +39,22 @@ const getTimeOfDay = (date) => {
   return "night";
 };
 
-const showGreeting = (date) => {
+const showGreeting = () => {
+  const date = new Date();
   const timeOfDay = getTimeOfDay(date);
   greetingContainer.textContent = `Good ${timeOfDay}`;
+};
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export const setBackground = () => {
+  const randomInt = getRandomInt(0, 20);
+  const randomIntToString = String(randomInt).padStart(2, 0);
+  const backgroundUrl = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${getTimeOfDay()}/${randomIntToString}.jpg')`;
+
+  body.style.backgroundImage = backgroundUrl;
 };
