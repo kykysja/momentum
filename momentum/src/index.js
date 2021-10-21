6ee963464683bc75a4c989545da7086de2647184
kykysja <kykysja11@gmail.com>
2021-10-21 21:46:26 +0200

    feat: implement saving username in Local Storage

--- momentum/src/index.js
+++ momentum/src/index.js
@@ -1,3 +1,9 @@
 import { handleTimeChanging } from "./js/calendar";
 
+import { getUserNameFromLocalStorage } from "./js/user-name";
+import { setUserNameToLocalStorage } from "./js/user-name";
+
+window.addEventListener("load", getUserNameFromLocalStorage);
+window.addEventListener("beforeunload", setUserNameToLocalStorage);
+
 handleTimeChanging();
