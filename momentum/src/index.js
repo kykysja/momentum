b11cedfe8e81e8eb3a30afb1afc2b310bc8a62b1
kykysja <kykysja11@gmail.com>
2021-10-22 11:46:44 +0200

    feat: implement the weather widget functionality

--- momentum/src/index.js
+++ momentum/src/index.js
@@ -3,18 +3,33 @@ import { showDate } from "./js/calendar";
 import { showGreeting } from "./js/greeting";
 import { getUserNameFromLocalStorage } from "./js/greeting";
 import { setUserNameToLocalStorage } from "./js/greeting";
+import { getCityFromLocalStorage } from "./js/weather";
+import { setCityToLocalStorage } from "./js/weather";
 import { setBackground } from "./js/bg-slider";
+import { getWeather } from "./js/weather";
 
-window.addEventListener("load", getUserNameFromLocalStorage);
-window.addEventListener("beforeunload", setUserNameToLocalStorage);
+const getDataFromLocalStorage = () => {
+  getUserNameFromLocalStorage();
+  getCityFromLocalStorage();
+};
+const setDataToLocalStorage = () => {
+  setUserNameToLocalStorage();
+  setCityToLocalStorage();
+};
+
+const handleWindowLoad = () => {
+  getDataFromLocalStorage();
+  getWeather();
+  setBackground();
+  handleTimeChanging();
+};
+
+window.addEventListener("load", handleWindowLoad);
+window.addEventListener("beforeunload", setDataToLocalStorage);
 
 const handleTimeChanging = () => {
   showTime();
   showDate();
   showGreeting();
-
   setTimeout(handleTimeChanging, 1);
 };
-
-setBackground();
-handleTimeChanging();
---
+++ momentum/src/js/weather.js
@@ -0,0 +1,48 @@
+const cityInput = document.querySelector(".city");
+const weatherIcon = document.querySelector(".weather-icon");
+const temperature = document.querySelector(".temperature");
+const weatherDescription = document.querySelector(".weather-description");
+const wind = document.querySelector(".wind");
+const humidity = document.querySelector(".humidity");
+
+export async function getWeather() {
+  weatherIcon.className = "weather-icon owf";
+
+  if (cityInput.value) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&lang=en&appid=026c2fee3b41cefa7680ac5c131b232d&units=metric`;
+    const res = await fetch(url);
+
+    if (res.status === 200) {
+      const data = await res.json();
+      weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+      temperature.textContent = `${Math.round(data.main.temp)}°C`;
+      weatherDescription.textContent = data.weather[0].description;
+      wind.textContent = `Wind speed: ${Math.round(data.wind.speed)} m/s`;
+      humidity.textContent = `Humidity: ${Math.round(data.main.humidity)}%`;
+    } else if (res.status === 404) {
+      temperature.textContent = "City not found";
+      weatherDescription.textContent = "";
+      wind.textContent = "";
+      humidity.textContent = "";
+    }
+  } else {
+    temperature.textContent = "Nothing to geocode for";
+    weatherDescription.textContent = "";
+    wind.textContent = "";
+    humidity.textContent = "";
+  }
+}
+
+export const setCityToLocalStorage = () => {
+  localStorage.setItem("city", cityInput.value);
+};
+
+export const getCityFromLocalStorage = () => {
+  if (localStorage.getItem("city")) {
+    cityInput.value = localStorage.getItem("city");
+  } else {
+    cityInput.value = "Minsk";
+  }
+};
+
+cityInput.addEventListener("change", getWeather);
